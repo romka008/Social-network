6338bda4aca5d0dfeeebdc7f18335ce88a8eeb00
Roman <romka008@yandex.ru>
2021-08-08 18:04:09 +0400

    Added main files

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+function Header() {
+    return <header className='header'>
+        <img src="https://img2.freepng.ru/20180509/gvw/kisspng-globe-earth-clip-art-5af366a46f1587.271956161525900964455.jpg" alt="" />
+    </header>
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+function Profile() {
+    return <div className='content'>
+    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/lake-large.jpg" alt="" />
+    <div>
+    ava + description
+  </div>
+  <div>
+    My posts
+    <div>
+      New post
+    </div>
+    <div>
+      <div>
+        post 1
+      </div>
+      <div>
+        post 2
+      </div>
+    </div>
+  </div>
+  </div>
+}
+
+export default Profile
