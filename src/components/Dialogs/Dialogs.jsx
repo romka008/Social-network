2fe9f38cc6b84893aace27a25621f048391f430b
Roman <romka008@yandex.ru>
2021-08-12 20:41:36 +0400

    21 Layout the Dialogues page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,8 +1,21 @@
 import s from './Dialogs.module.css';
 
 function Dialogs(props) {
-  return <div>
-    Dialogs
+  return <div className={s.dialogs}>
+    <div className={s.dialogItems}>
+      <div className={s.dialog}>Sergey</div>
+      <div className={s.dialog}>Andrey</div>
+      <div className={s.dialog}>John</div>
+      <div className={s.dialog}>Ou Kee</div>
+      <div className={s.dialog}>No mercy</div>
+      <div className={s.dialog}>Kenjoh</div>
+      <div className={s.dialog}>Roman</div>
+    </div>
+    <div className={s.messages}>
+      <div className={s.message}>What is it?</div>
+      <div className={s.message}>Where are u?</div>
+      <div className={s.message}>Yo</div>
+    </div>
   </div>
 }
 
